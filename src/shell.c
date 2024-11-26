120192263df381237590adbee9a6d76bb903cbda
tylercarver16 <tbc22a@fsu.edu>
2024-11-25 21:31:57 -0500

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -36,3 +36,49 @@ typedef struct __attribute__((packed))
     unsigned char empty[420];
     unsigned short Signature_word;
 } BPB;
+
+// Function to print BPB information
+void print_bpb_info(BPB *bpb, FILE *fp) {
+    // total clusters in data region
+    unsigned int totalClusters = bpb->BPB_TotSec32 - bpb->BPB_RsvdSecCnt - (bpb->BPB_NumFATs * bpb->BPB_FATSz32);
+
+    // print info
+    printf("Root cluster position (in cluster #): %u\n", bpb->BPB_RootClus);
+    printf("Bytes per sector: %u\n", bpb->BPB_BytesPerSec);
+    printf("Sectors per cluster: %u\n", bpb->BPB_SecsPerClus);
+    printf("Total clusters in data region: %u\n", totalClusters);
+    printf("Number of entries in one FAT: %u\n", ((bpb->BPB_FATSz32 * bpb->BPB_BytesPerSec) / 4));
+    printf("Size of image (in bytes): %u\n", (bpb->BPB_TotSec32 * bpb->BPB_BytesPerSec));
+}
+
+int main(int argc, char *argv[]){
+        if (argc != 2) {
+        fprintf(stderr, "Usage: %s [FAT32 ISO file]\n", argv[0]);
+        return 1;
+    }
+    if (access(argv[1], F_OK) == -1) {
+        perror("Error");
+        return 1;
+    }
+    FILE *fp = fopen(argv[1], "rb");
+    if (!fp) {
+        perror("Error opening the image file");
+        return 1;
+    }
+
+    BPB bpb;
+    fseek(fp, 0, SEEK_SET);
+    if (fread(&bpb, sizeof(BPB), 1, fp) != 1) {
+        perror("Failed to read BPB structure");
+        fclose(fp);
+        return 1;
+    }
+
+    // shell interface
+    while(1){
+        print_bpb_info(&bpb, fp);
+        break;
+    }
+
+    return 0;
+}
