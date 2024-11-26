f22287b184d4b0bcd30f0da1b9b4c8c92771a175
tylercarver16 <tbc22a@fsu.edu>
2024-11-25 21:28:11 -0500

    Create shell.c

---
+++ src/shell.c
@@ -0,0 +1,38 @@
+#include "lexer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// BPB structure
+typedef struct __attribute__((packed))
+{
+    unsigned char BS_jmpBoot[3];
+    unsigned char BS_OEMName[8];
+    unsigned short BPB_BytesPerSec;
+    unsigned char BPB_SecsPerClus;
+    unsigned short BPB_RsvdSecCnt;
+    unsigned char BPB_NumFATs;
+    unsigned short BPB_RootEntCnt;
+    unsigned short BPB_TotSec16;
+    unsigned char BPB_Media;
+    unsigned short BPB_FATSz16;
+    unsigned short BPB_SecPerTrk;
+    unsigned short BPB_NumHeads;
+    unsigned int BPB_HiddSec;
+    unsigned int BPB_TotSec32;
+    unsigned int BPB_FATSz32;
+    unsigned short BPB_ExtFlags;
+    unsigned short BPB_FSVer;
+    unsigned int BPB_RootClus;
+    unsigned short BPB_FSInfo;
+    unsigned short BPB_BkBootSe;
+    unsigned char BPB_Reserved[12];
+    unsigned char BS_DrvNum;
+    unsigned char BS_Reserved1;
+    unsigned char BS_BootSig;
+    unsigned int BS_VollD;
+    unsigned char BS_VolLab[11];
+    unsigned char BS_FilSysType[8];
+    unsigned char empty[420];
+    unsigned short Signature_word;
+} BPB;
